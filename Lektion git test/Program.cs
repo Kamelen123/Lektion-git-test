dd5123ccdd97b69876e612b0f40212adcecf5803
Torbjörn <torbjorn.rodstrom@gmail.com>
2023-09-25 13:30:49 +0200

    Add project files.

---
+++ Lektion git test/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Lektion_git_test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
